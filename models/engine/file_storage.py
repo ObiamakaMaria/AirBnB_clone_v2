6da17b6f74abde84f34bab056b728f80739f8fb6
ObiamakaMaria <obiamaka0101@gmail.com>
2023-11-22 07:52:29 -0800

    Updated_file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,20 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
-    """
-    Returns a dictionary of objects currently in storage.
-
-    If cls is provided, returns a dictionary of objects of the specified class.
-    """
-    if cls is not None:
-        some_dict = {}
-        for key, obj in self.__objects.items():
-            if isinstance(obj, cls):
-                some_dict[key] = obj
-        return some_dict
-    else:
-        return self.__objects
+    def all(self):
+    """ Returns a dictionary of models currently in storage """
+    return FileStorage.__objebove cts
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
