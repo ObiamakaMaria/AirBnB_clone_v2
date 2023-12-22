c315340ced934d2a7ffbadf294285286e73c10e2
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-22 06:05:13 -0800

    4-number_route.py

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This script starts a Flask web application."""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_text(text):
+    text = text.replace('_', ' ')
+    result_text = 'C {}'.format(text)
+    return result_text
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<string:text>", strict_slashes=False)
+def python_text(text='is cool'):
+
+    text = text.replace('_', ' ')
+    format_text = 'Python {}'.format(text)
+    return format_text
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_check(n):
+    return '{} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
