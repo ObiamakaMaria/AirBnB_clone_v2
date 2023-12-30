7f6e8eb88c32f0b706a5d2888726873089b64a2f
ObiamakaMaria <obiamaka0101@gmail.com>
2023-12-29 18:33:21 -0800

    updated db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -13,60 +13,60 @@ from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 
 
 class DBStorage:
-    """ The database storage engine
-    Attributes:
-	__engine (sqlalchemy Engine): Sqlalchemy Engine used.
-	__session(sqlalchemy Session): the sqlalchemy session.
-    """
+    """The database storage engine."""
     __engine = None
     __session = None
 
     def __init__(self):
-        """ initialize a new storage instance."""
+        """Initialize a new storage instance."""
         self.__engine = create_engine(
-		"mysql+mysqldb://{}:{}@{}/{}".format(
-		    getenv("HBNB_MYSQL_USER"),
-		    getenv("HBNB_MYSQL_PWD"),
-		    getenv("HBNB_MYSQL_HOST"),
-		    getenv("HBNB_MYSQL_DB")
-		    ),
-		pool_pre_ping=True
-		) 
+            "mysql+mysqldb://{}:{}@{}/{}".format(
+                getenv("HBNB_MYSQL_USER"),
+                getenv("HBNB_MYSQL_PWD"),
+                getenv("HBNB_MYSQL_HOST"),
+                getenv("HBNB_MYSQL_DB")
+            ),
+            pool_pre_ping=True
+        )
         if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """ method to query on current db session """
+        """Method to query on current db session."""
         if cls is None:
             obj = []
             for model in [State, City, User, Place, Review, Amenity]:
                 try:
-                   obj.extend(self.__session.query(model).all())
+                    obj.extend(self.__session.query(model).all())
                 except Exception as e:
-                   print(f"Error querying {model.__name__}: {e}")
+                    print(f"Error querying {model.__name__}: {e}")
         else:
             if type(cls) == str:
                 cls = eval(cls)
             obj = self.__session.query(cls)
-        
+
         return {"{}.{}".format(type(v).__name__, v.id): v for v in obj}
 
     def new(self, obj):
-        """ Method Add obj to the current db session"""
+        """Method to add obj to the current db session."""
         self.__session.add(obj)
 
     def save(self):
-        """ method that commit all changes to the current db session"""
+        """Method that commits all changes to the current db session."""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """Method that delete an object from the current db sessin"""
+        """Method that deletes an object from the current db session."""
         if obj is not None:
-             self.__session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
-        """method that created all tables in the db and create a new session."""
+        """Method that creates all tables in the db & create a new session."""
         Base.metadata.create_all(self.__engine)
         s_f = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(s_f)
         self.__session = Session()
+
+    def close(self):
+        """Call close() method on the private session attribute."""
+        self.__session.close()
