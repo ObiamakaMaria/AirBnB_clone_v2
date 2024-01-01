a8af13c4a71926f5ea503edbd5d5584e2ec82fc5
ObiamakaMaria <obiamaka0101@gmail.com>
2024-01-01 06:40:51 -0800

    updated_file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,7 +25,6 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             self.__objects[key] = obj
-        
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -66,3 +65,7 @@ class FileStorage:
             del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
         except (AttributeError, KeyError):
             pass
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
